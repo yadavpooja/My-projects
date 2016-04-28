8f014f634b3ab15ffc66b85a347ecdb47f090000
Pooja Yadav <ypooja510@gmail.com>
2016-04-28 19:54:00 +0530

    add hello design program

---
+++ hello_design.py
@@ -0,0 +1,18 @@
+def hello():
+    print "*       *\n" * 3,"* " *5,"\n*       *" * 3
+    print " "
+    print "* " * 5, "\n*" * 3,"* " * 4,"\n*" *3,"* " * 4
+    print " "
+    print "*\n" *  6,"* " * 5
+    print " "
+    print "*\n" *  6,"* " * 5
+    print " "
+    print "* " * 5,"\n*       *" * 5," \n","* " * 5
+
+
+
+
+
+
+
+hello()
