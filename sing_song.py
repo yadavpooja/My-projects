b14cf34e21d60cca0981d613651a6e4f217367a5
Pooja Yadav <ypooja510@gmail.com>
2016-05-10 11:51:59 +0530

    add sing a song programme

---
+++ sing_song.py
@@ -0,0 +1,21 @@
+class Song():
+    def __init__(self,lyrics):
+        self.lyrics = lyrics
+
+
+    def sing(self):
+        return self.lyrics
+
+
+
+
+
+
+Birthday = Song("Happy B'day To you\nMay god bless u,\nHappy b'day to you....")
+print(Birthday.sing())
+
+
+Star_poem = Song("\nTwinkle Twinkle little star\nHow I wonder what you are\nUp above the world so high,\nlike a diamond in the sky")
+print(Star_poem.sing())
+
+
