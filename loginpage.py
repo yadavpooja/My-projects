71037e43e1b0b8edc7820cc3f566e72f58dcaf45
Pooja Yadav <ypooja510@gmail.com>
2016-05-16 09:21:02 +0530

    add login programme

--- loginpage.py
+++ loginpage.py
@@ -1,34 +1,37 @@
-def login():
+def login(username, password, data):
+    if name in data:
+        if data[name] == password:
+            return 0
+        else:
+            return 1
+    return 2
+
+def register(username, password, data):
+    if name not in data:
+        data[name] = password
+    return data
+
+
+if __name__ == '__main__':
     Flag = True
     data = {"riya": "123"}
     while Flag:
         name = raw_input("\nPlease enter your user name: ")
-        if name in data:
-            pp = raw_input("Please enter your password: ")
-            if pp == data[name]:
-                print "Logged in successfully.\n Enjoy different services."
-            else:
-                print "\nPlease enter a valid password."
         if name == "quit":
             Flag = False
             print"You exit"
-
-        elif name not in data:
-            print("Hello new user")
-            user = raw_input("""Do you want to create your account? \nType y or yes to continue. """)
-            if user == "y" or user == "yes":
-                user1 = raw_input("\nPlease enter your user name? ")
-                pp1 = raw_input("Please enter your password? ")
-                data[user1] = pp1
-                print("Your account created succesfully,enjoy!!")
-                print("Login and continue or type quit to exit.\n Thank You.")
-
-
-
-
-
-
-
-
-
-login()
+        else:
+            pp = raw_input("Please enter your password: ")
+            login_result = login(name, pp, data)
+            #import pdb; pdb.set_trace()
+            if not login_result:
+                print "Logged in successfully.\n Enjoy different services."
+            elif login_result == 1:
+                print "\nPlease enter a valid password."
+            else:
+                print("Hello new user")
+                user = raw_input("""Do you want to create your account? \nType y or yes to continue. """)
+                if user == "y" or user == "yes":
+                    data = register(name, pp, data)
+                    print("Your account created succesfully,enjoy!!")
+                    print("Login and continue or type quit to exit.\n Thank You.")
