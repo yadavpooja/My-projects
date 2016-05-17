5693a3aadd76e60bd5ba0b4ac3176dda8e3fb92d
Pooja Yadav <ypooja510@gmail.com>
2016-05-17 14:43:30 +0530

    add programme to claculate distance

--- distance_cal.py
+++ distance_cal.py
@@ -1,34 +1,53 @@
 #calculate the distance from the origin#
-
-
-
-
+import math
 class Distance():
-    def __init__(self,x= 0,y = 0):
+    def __init__(self,name,x= 0,y = 0):
+            self.name = name
             self.x = x
             self.y = y
 
     def get_distance(self):
-        return ((self.x **2) + (self.y ** 2)) ** 0.5
+        return int(((self.x **2) + (self.y ** 2)) ** 0.5)
 
 
-def dis():
+def dis(name, x,y):
+    try:
+        x = int(x)
+        y = int(y)
+        a = Distance(name,x,y)
+        ans = a.get_distance()
+        if x == 0 or y == 0:
+            return 0
+        else:
+            return ans
+    except ValueError:
+        return 1
+    except SyntaxError:
+        return 2
+
+
+if __name__ == "__main__":
     Flag = True
     while Flag:
         name = raw_input("Enter the name: ")
-        try:
-            x = int(input("Enter your first co-ordinate: "))
-            y = int(input("Enter your second co-ordinate: "))
-            if x == 0 or y == 0:
-                Flag = False
-            else:
-                a = Distance(x,y)
-                print ("%s have to cover %d km distance." %(name.title(),a.get_distance()))
-
-        except NameError:
+        #try:
+        x = raw_input("Enter your first co-ordinate: ")
+        y = raw_input("Enter your second co-ordinate: ")
+        result = dis(name, x,y)
+        if not result:
+            Flag = False
+        elif result == 1:
             print("Enter a integer")
-        except SyntaxError:
+        elif result == 2:
             print("Enter a integer")
+        else:
+           # a = Distance(x,y)
+            print ("%s have to cover %d km distance." %(name.title(),result))
+
+       # except NameError:
+        #    print("Enter a integer")
+       # except SyntaxError:
+        #    print("Enter a integer")
 
 
-dis()
+
